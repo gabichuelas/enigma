c411b4990e61e8c83f43a54c4937551cc5abcf98
Gaby Mendez <gmendez90@gmail.com>
2020-06-05 23:03:09 -0400

    Write initial tests for Enigma

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,6 +1,60 @@
 require "minitest/autorun"
 require "minitest/pride"
+require "date"
 require "./lib/enigma"
 
 class EnigmaTest < Minitest::Test
-end 
+  def setup
+    @enigma = Enigma.new
+  end
+
+  def test_it_exists
+    assert_instance_of Enigma, @enigma
+  end
+
+  def test_can_encrypt
+    expected = {
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+    }
+
+    assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
+  end
+
+  def test_can_decrypt
+    skip
+    expected = {
+      decryption: "hello world",
+      key: "02715",
+      date: "040895"
+    }
+
+    assert_equal expected, @enigma.decrypt("keder ohulw", "02715", "040895")
+  end
+
+  def test_can_encrypt_using_generated_date
+    skip
+    encrypted = {
+
+    }
+    assert_equal encrypted, @enigma.encrypt("hello world", "02715")
+  end
+
+  def test_can_decrypt_using_generated_date
+    skip
+    encrypted = enigma.encrypt("hello world", "02715")
+    decrypted = {
+
+    }
+    assert_equal decrypted, @enigma.decrypt(encrypted[:encryption], "02715")
+  end
+
+  def test_can_encrypt_using_generated_key_date
+    skip
+    encrypted = {
+
+    }
+    assert_equal encrypted, @enigma.encrypt("hello world")
+  end
+end
