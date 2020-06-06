079597d8451efcf9569cd16b47aac280d8f3ea79
Gaby Mendez <gmendez90@gmail.com>
2020-06-06 00:25:02 -0400

    Write #shifts(key, date) method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -15,7 +15,12 @@ class Enigma
   end
 
   def shifts(key, date)
-    # index math to find new character
+    last_four = (date.to_i ** 2).to_s[-4..-1]
+    a = key[0..1].to_i + last_four[0].to_i
+    b = key[1..2].to_i + last_four[1].to_i
+    c = key[2..3].to_i + last_four[2].to_i
+    d = key[3..4].to_i + last_four[3].to_i
+    { a: a, b: b, c: c, d: d }
   end
 
 end
