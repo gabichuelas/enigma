2c0e05313b12976619fe118223d0abe9f778dbb9
Gaby Mendez <gmendez90@gmail.com>
2020-06-07 15:47:56 -0400

    Add more assertions to Key test

--- test/key_test.rb
+++ test/key_test.rb
@@ -19,5 +19,8 @@ class KeyTest < Minitest::Test
 
     Key.stubs(:rand).returns("721")
     assert_equal "00721", Key.make
+
+    Key.stubs(:rand).returns("7")
+    assert_equal "00007", Key.make
   end
 end
