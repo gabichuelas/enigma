f6f66f196b8cafd425a02cd70f4175f579e2d2e6
Gaby Mendez <gmendez90@gmail.com>
2020-06-07 15:37:01 -0400

    Refactor #new_char and #og_char into one method #shift, delete unecessary tests

--- lib/encryptable.rb
+++ lib/encryptable.rb
@@ -3,11 +3,24 @@ require './lib/key'
 
 module Encryptable
   KEY = Key.make
+  INDEX_RANGES = [0..1, 1..2, 2..3, 3..4]
   DATE = Date.today.strftime("%d%m%y")
   ALPHA = ("a".."z").to_a << " "
 
   def prepare(message)
     split_chars = message.downcase.chars
     split_chars.each_slice(4)
+  end
+
+  def last_four(date)
+    (date.to_i ** 2).to_s[-4..-1]
+  end
+
+  def create_shifts(key, date)
+    INDEX_RANGES.each_with_index.reduce({}) do |shifts, (range, i)|
+      shifts["#{i}"] ||= 0
+      shifts["#{i}"] = key[range].to_i + last_four(date)[i].to_i
+      shifts
+    end
   end
 end
--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,7 +2,7 @@ require_relative 'cipher'
 
 class Enigma < Cipher
   def encrypt(message, key = KEY, date = DATE)
-    encrypted = shift_chars(message, key, date)
+    encrypted = encrypt_chars(message, key, date)
     {
       encryption: encrypted.join,
       key: key,
@@ -11,7 +11,7 @@ class Enigma < Cipher
   end
 
   def decrypt(message, key = KEY, date = DATE)
-    decrypted = unshift_chars(message, key, date)
+    decrypted = decrypt_chars(message, key, date)
     {
       decryption: decrypted.join,
       key: key,
