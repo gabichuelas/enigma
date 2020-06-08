64a5e1fa14921233982d6ccd12a0bcc1f439f81b
Gaby Mendez <gmendez90@gmail.com>
2020-06-08 18:03:06 -0400

    Edit EnigmaRunner and encrypt

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,15 +1,9 @@
 require_relative 'enigma_runner'
+require_relative 'encryptable'
 
 runner = EnigmaRunner.new
-required = ARGV[0] && ARGV[1]
 
-if required && ARGV[2] && ARGV[3]
-  runner.run_encryption(ARGV[0], ARGV[1], ARGV[2], ARGV[3])
-elsif required && ARGV[2]
-  runner.run_encryption(ARGV[0], ARGV[1], ARGV[2], DATE)
-elsif required && ARGV[3]
-  runner.run_encryption(ARGV[0], ARGV[1], KEY, ARGV[3])
-elsif required
+if ARGV[0] && ARGV[1]
   runner.run_encryption(ARGV[0], ARGV[1])
 else
   puts "Wrong number of arguments, try again!"
--- lib/enigma_runner.rb
+++ lib/enigma_runner.rb
@@ -2,15 +2,15 @@ require_relative 'enigma'
 
 class EnigmaRunner < Enigma
 
-  def run_encryption(input, output, key = KEY, date = DATE)
+  def run_encryption(input, output)
     message = open_read_close(input)
-    encryption = encrypt(message, key, date)
+    encryption = encrypt(message)
     write_to_new_file(output, encryption[:encryption])
 
     puts "Created '#{output}' with the key #{encryption[:key]} and date #{encryption[:date]}"
   end
 
-  def run_decryption(input, output, key = KEY, date = DATE)
+  def run_decryption(input, output, key, date)
   end
 
   def open_read_close(file)
