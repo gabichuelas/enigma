5179b1a029c8ddd44c0a6be7dd619042a1694b41
Gaby Mendez <gmendez90@gmail.com>
2020-06-09 12:02:43 -0400

    Refactor #encrypt_chars and #decrypt_chars to use times loop

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -6,22 +6,20 @@ class Cipher
   def encrypt_chars(message, key, date)
     shift_by = create_shifts(key, date)
     prepare(message).reduce([]) do |acc, char|
-        acc << shift(char[0], shift_by["0"])
-        acc << shift(char[1], shift_by["1"]) if char[1]
-        acc << shift(char[2], shift_by["2"]) if char[2]
-        acc << shift(char[3], shift_by["3"]) if char[3]
-        acc
+      4.times do |i|
+        acc << shift(char[i], shift_by["#{i}"]) if char[i]
+      end
+      acc
     end
   end
 
   def decrypt_chars(message, key, date)
     shift_by = create_shifts(key, date)
     prepare(message).reduce([]) do |acc, char|
-        acc << shift(char[0], -shift_by["0"])
-        acc << shift(char[1], -shift_by["1"]) if char[1]
-        acc << shift(char[2], -shift_by["2"]) if char[2]
-        acc << shift(char[3], -shift_by["3"]) if char[3]
-        acc
+      4.times do |i|
+        acc << shift(char[i], -shift_by["#{i}"]) if char[i]
+      end
+      acc
     end
   end
 
